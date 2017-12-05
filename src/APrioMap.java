d160f50c4c8ae6695423e14e3e8a05429b9621d6
Matilda Idoffsson <matido@student.chalmers.se>
2017-12-05 14:05:39 +0100

    Lagt in kommentar

--- src/APrioMap.java
+++ src/APrioMap.java
@@ -1,5 +1,12 @@
-
+/*
+put(k, v) lägger till nyckelvärdeparet <k, v>. Om nyckel k redan är associerad med ett värde, säg vg, så ska k hädanefter istället vara associerad med v. I detta fall får man tänka på att om v < vg eller v > vg så kan utbytet av värde innebära att kö-placeringen ändras.
+get(k) returnerar det värde som är associerat med nyckeln k, eller null och k inte är associerad med ett värde.
+peek() returnerar det (eller ett av de) nyckel-värdepar som har högst prioritet (minst värde). Paret tas inte bort.
+poll() returnerar och tar bort det nyckel-värdepar som har högst prioritet.
+ */
 
 
 public class APrioMap  {
+
+
 }
