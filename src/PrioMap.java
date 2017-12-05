86503c9c91fa0a3e34385d8c955228ad0e0d9f8a
Matilda Idoffsson <matido@student.chalmers.se>
2017-12-05 16:32:36 +0100

    Lite uppdaterat

--- src/PrioMap.java
+++ src/PrioMap.java
@@ -1,8 +1,7 @@
-import javafx.util.Pair;
 
 public interface PrioMap<K, V extends Comparable<? super V>> {
     void put(K k, V v);
     V get(K k);
     Pair<K, V> peek();
-    Pair poll();
+    Pair<K, V> poll();
 }
